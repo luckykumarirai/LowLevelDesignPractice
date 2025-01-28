9062094504ca39815e9f49c552cf63ca5e52fde2
luckykumarirai <luckykumari112000@gmail.com>
2025-01-28 23:48:59 +0530

    prototype design pattern

--- SingltonDesign/examples.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-#include "samosa.cpp"
-
-int main() {
-    Samosa *s1 = Samosa::getSamosa();
-    Samosa *s2 = Samosa::getSamosa();
-    Samosa *s3 = Samosa::getSamosa();
-};
